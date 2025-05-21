41b9d83764f4d44882595146528369e8df803557
ChoiDoyun <vahyeon2509@sonline20.sen.go.kr>
2025-05-21 11:34:50 +0900

    38

--- Assets/2_Scripts/CrashDeTector.cs
+++ Assets/2_Scripts/CrashDeTector.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Crash : MonoBehaviour
+public class CrashDetector : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
             Debug.Log("오! 내머리야!");
+            SceneManager.LoadScene(0);
         }
     }
 }
